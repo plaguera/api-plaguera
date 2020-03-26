f48cc6167e10b65fd1c786dde2948aae0f128db6
Pedro Lagüera <laguerapedro@gmail.com>
2020-03-26 22:46:31 +0000

    Basic Structure

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+require('dotenv').config();
+import http from 'http';
+import express from 'express';
+
+const router = express();
+
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+	console.log(`Server is running http://localhost:${PORT}...`)
+);
